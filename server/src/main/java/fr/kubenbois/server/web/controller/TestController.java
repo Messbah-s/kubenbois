2137b7e0f0de54d4d0aecad6925fb8ec3d686896
MessbahSamir <samir.messbah@vaxa.fr>
2025-10-03 01:55:33 +0200

    Ajout de l'url frontend application-prod.properties

--- server/src/main/java/fr/kubenbois/server/web/controller/TestController.java
+++ server/src/main/java/fr/kubenbois/server/web/controller/TestController.java
@@ -1,11 +1,9 @@
 package fr.kubenbois.server.web.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin("*")
 public class TestController {
 
     @GetMapping("/hello")
